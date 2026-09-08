d9a4b19347bbead36bf2f8626017c180bd2b0e18
afiss859-eng <afiss859@gmail.com>
2026-09-08 14:56:17 +0100

    feat(orange): add startup runtime for automatic updates

--- orange-money/app/build.gradle.kts
+++ orange-money/app/build.gradle.kts
@@ -23,11 +23,11 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.18.0")
     implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.startup:startup-runtime:1.2.0")
     implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
